4f1259796f1a67caae6fd5d5a2a15d925506636c
matosluciano <luciano7matos@gmail.com>
2024-05-18 03:27:07 -0300

    image field

--- products/views.py
+++ products/views.py
@@ -1,7 +1,20 @@
-from django.views.generic import ListView
-from django.shortcuts import render
+from django.http import Http404
+
+from django.views.generic import ListView, DetailView
+from django.shortcuts import render, get_object_or_404
 
 from .models import Product
+
+class ProductFeaturedListView(ListView):
+    template_name = "products/list.html"
+    
+    def get_queryset(self, *args, **kwargs):
+        request = self.request
+        return Product.objects.featured()
+
+class ProductFeaturedDetailView(DetailView):
+    queryset = Product.objects.all().featured()
+    template_name = "products/featured-detail.html"
 
 #Class Based View
 class ProductListView(ListView):
@@ -22,3 +35,31 @@ def product_list_view(request):
         'object_list': queryset
     }
     return render(request, "products/list.html", context)
+
+#Class Based View
+class ProductDetailView(DetailView):
+    template_name = "products/detail.html"
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
+        print(context)
+        return context
+
+    def get_object(self, *args, **kwargs):
+        pk = self.kwargs.get('pk')
+        instance = Product.objects.get_by_id(pk)
+        if instance is None:
+            raise Http404("Esse produto não existe!")
+        return instance
+
+#Function Based View
+def product_detail_view(request, pk = None, *args, **kwargs):
+    instance = Product.objects.get_by_id(pk)
+    print(instance)
+    if instance is None:
+        raise Http404("Esse produto não existe!")
+
+    context = {
+        'object': instance
+    }
+    return render(request, "products/detail.html", context)
