f466fa86e91f6d5eeb21be05cee5950ed426df17
matosluciano <luciano7matos@gmail.com>
2024-05-24 00:03:22 -0300

    finalização do checkout

--- newstore/settings.py
+++ newstore/settings.py
@@ -44,6 +44,8 @@ INSTALLED_APPS = [
     'carts',
     'orders',
     'accounts',
+    'billing',
+    'addresses',
 ]
 
 MIDDLEWARE = [
--- newstore/urls.py
+++ newstore/urls.py
@@ -6,8 +6,9 @@ from django.contrib.auth.views import LogoutView
 from django.urls import path, include
 from django.views.generic import TemplateView
 from carts.views import cart_home
-from accounts.views import login_page, register_page, logout_page
-from .views import (home_page, 
+from accounts.views import login_page, register_page, logout_page, guest_register_view
+from addresses.views import checkout_address_create_view, checkout_address_reuse_view
+from .views import (home_page,  
                     about_page, 
                     contact_page
 )
@@ -17,7 +18,10 @@ urlpatterns = [
     path('about/', about_page, name='about'),
     path('contact/', contact_page, name='contact'),
     path('cart/', include("carts.urls", namespace="cart")),
+    path('checkout/address/create/', checkout_address_create_view, name='checkout_address_create'),
+    path('checkout/address/reuse/', checkout_address_reuse_view, name='checkout_address_reuse'),
     path('login/', login_page, name='login'),
+    path('register/guest/', guest_register_view, name='guest_register'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', register_page, name='register'),
     path('bootstrap/', TemplateView.as_view(template_name='bootstrap/example.html')),
